3c72ee7a24444f6ac42b9eaed01d00442702fba9
Eugene Popov <kaaboeld@gmail.com>
2025-12-21 22:56:25 +0100

    Fix DS-223 styles for table view

--- components/ui/table/TableCell.vue
+++ components/ui/table/TableCell.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
     data-slot="table-cell"
     :class="
       cn(
-        'p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
+        'p-2 align-middle whitespace-nowrap border-x border-b border-gray-200 [&:has([role=checkbox])]:pr-2 [&>[role=checkbox]]:translate-y-[2px]',
         props.class,
       )
     "
